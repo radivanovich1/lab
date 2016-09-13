d6ccd42510093aacd2843121dc15c7d4237f65b6
radivanovich1 <radivanovich1@gmail.com>
2016-09-13 14:27:51 +0300

    calc

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double oper1;
+            double oper2;
+            double res;
+
+            Console.WriteLine("Выберите операцию: 1 - сложение 2 - вычитание 3 - умножения 4 - деление ");
+            try
+            {
+                int caseSwitch = Convert.ToInt16(Console.ReadLine());
+                if (caseSwitch < 1 || caseSwitch>4)   // 4 операции
+                {
+                    Console.WriteLine("Неверные данные");
+                }
+
+                switch (caseSwitch)
+                {
+                    case 1:                       
+                        Console.WriteLine("Введите первое число:");
+                        oper1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите второе число:");
+                        oper2 = Convert.ToDouble(Console.ReadLine());
+                        Calc sum = new Calc();
+                        res = Calc.Sum(oper1, oper2);
+                        Console.WriteLine("Результат " + res);
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите первое число:");
+                        oper1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите второе число:");
+                        oper2 = Convert.ToDouble(Console.ReadLine());
+                        Calc min = new Calc();
+                        res = Calc.Sub(oper1, oper2);
+                        Console.WriteLine("Результат " + res);
+
+                        break;
+                    case 3:
+                        Console.WriteLine("Введите первое число:");
+                        oper1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите второе число:");
+                        oper2 = Convert.ToDouble(Console.ReadLine());
+                        Calc umn = new Calc();
+                        res = Calc.Mult(oper1, oper2);
+                        Console.WriteLine("Результат " + res);
+
+
+                        break;
+                    case 4:
+                        Console.WriteLine("Введите первое число:");
+                        oper1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите второе число:");
+                        oper2 = Convert.ToDouble(Console.ReadLine());
+                        Calc del = new Calc();
+                        res = Calc.Div(oper1, oper2);
+                        Console.WriteLine("Результат " + res);
+
+
+                        break;
+
+                }
+
+
+            }
+            catch {
+                Console.WriteLine("Упс!!! Неверные данные.");
+            }
+
+
+        }
+          
+    }
+}
---
+++ ConsoleApplication1/fun.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+
+   public class Calc
+    {        
+
+        public static double Sum(double s1, double s2 )
+        {
+
+            double s = s1 + s2;
+            return s;
+        
+        }
+
+        public static double Sub(double m1, double m2)
+        {
+
+            double m = m1 - m2;
+            return m;
+
+        }
+
+        public static double Mult(double u1, double u2)
+        {
+
+            double u = u1 * u2;
+            return u;
+
+        }
+        public static double Div(double d1, double d2)
+        {
+
+            double d = d1/d2;
+            return d;
+
+        }
+
+        
+    }
+}
